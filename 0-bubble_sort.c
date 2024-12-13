aaa6e5fab58da18ed8420203b15c8ecdbb670a4d
root <root@DESKTOP-46AJ964>
2024-12-13 15:23:00 +0100

    exo

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,15 +12,18 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
+	size_t i;
+	size_t j;
+	size_t k;
 
-	for (size_t i = 0; i < size - 1; i++) {
-		for (size_t j = 0; j < size - i - 1; j++) {
+	for (i = 0; i < size - 1; i++) {
+		for (j = 0; j < size - i - 1; j++) {
 			if (array[j] > array[j + 1]) {
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-				for (size_t k = 0; k < size; k++) {
+				for (k = 0; k < size; k++) {
 					printf("%d ", array[k]);
 				}
 				printf("\n");
